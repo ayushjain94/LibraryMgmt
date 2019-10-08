7037493c68be511731d42f0fabe218a95d2c5f65
Ayush Jain <ayush@Ayushs-MacBook-Pro.local>
2019-10-08 03:31:13 -0400

    Mail Configurations for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,23 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "libraryMgmtSys_production"
 
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'jn.ayush@gmail.com',
+    :password             => 'ixwpemankgsqlqop',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+}
+
+  config.action_mailer.default_url_options = { :host => 'http://brainiacslms.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
