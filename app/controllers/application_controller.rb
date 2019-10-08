1fc8e274bffe5c0dfb9e4fd3d0f4c1815be0e632
Ayush Jain <ayush@Ayushs-MacBook-Pro.local>
2019-10-08 17:52:07 -0400

    get logged in user function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-    
+    before_action :get__loggedin_user
+
+    def get__loggedin_user
+        if admin_signed_in?
+            user = 'admin'
+        elsif librarian_signed_in?
+            user = 'librarian'
+        elsif student_signed_in?
+            user = 'student'
+        else
+            user = nil
+        end
+        @current_user = user
+    end
+
 end
